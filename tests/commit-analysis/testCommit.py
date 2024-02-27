bf61ff6e7ba94c3b0ef1c03284dba39dd92935dc
defleeratan <deflee@ibm.com>
2024-02-27 12:21:50 +0530

    Modified commit functions

--- tests/commit-analysis/testCommit.py
+++ tests/commit-analysis/testCommit.py
@@ -1,5 +1,9 @@
 def func_test_commit:
     print("Commit is being tested")
+    print("Commit 2 is being tested")
+    print("Commit 3 is being tested")
+    print("Commit 4 is being tested")
+    print("Commit 5 is being tested")
 
 def func_test_commit_msg:
     pass
